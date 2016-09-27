738afe8440e095a8b03a22fe15df63165c498f18
ivanseven <sir.ivanyeong@gmail.com>
2016-09-27 16:09:47 +0800

    Readded Missing files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,5 +41,16 @@ Rails.application.configure do
 
   # Clearance: Configure the mailer to create full URLs in emails
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.action_mailer.delivery_method = :letter_opener
+      # config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => ENV['gmail_username'],
+     :password             => ENV['gmail_password'],
+     :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
 
 end
